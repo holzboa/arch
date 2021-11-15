3ce60b79e5efab38c54b7428d9b52301a6a24cda
holzboa <59257180+holzboa@users.noreply.github.com>
2021-11-15 22:38:54 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -11,5 +11,9 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay
  
-yay -S autofs pamac-aur --noconfirm
+yay -S autofs pamac-aur bashmount dragon-drag-and-drop --noconfirm
+yay -S flacon gmusicbrowser-git ipscan jdownloader2 luckybackup --noconfirm
+yay -S menulibre moc-unstable mocp-themes-git mp3check --noconfirm 
+yay -S mp3directcut mp3gain mugshot nerd-fonts-hack --noconfirm
+yay -S puddletag zramswap usbtop thunar-custom-actions
 sudo systemctl enable autofs

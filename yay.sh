908b0806d3a81eed65bbcdb16ef645507047e48b
holzboa <59257180+holzboa@users.noreply.github.com>
2023-10-27 11:38:33 +0200

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -19,7 +19,7 @@
 yay -S --needed brave-bin bashmount create_ap dragon-drop etcher-bin --noconfirm 
 yay -S --needed flacon ios-mount-git ipscan jdownloader2 --noconfirm
 #yay -S --needed kde-servicemenus-flacconvert kde-servicemenus-imagetools kde-servicemenus-pdf kde-servicemenus-thunderbird-attachment kwin-effects-burn-my-windows-git --noconfirm
-yay -S --needed kodi-addon-pvr-iptvsimple koreader-bin losslesscut-bin --noconfirm
+yay -S --needed kodi-addon-pvr-iptvsimple kora-icon-theme koreader-bin losslesscut-bin --noconfirm
 yay -S --needed luckybackup menulibre mocp-themes-git mp3check --noconfirm 
 yay -S --needed mp3directcut mp3gain mugshot nerd-fonts-hack nomacs onlyoffice-bin --noconfirm 
 yay -S --needed peaclock puddletag pycp rofi-greenclip rpi-imager --noconfirm 

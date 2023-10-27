4ed4cd4bf4e093733613e85e5f7f34c86b4add0e
holzboa <59257180+holzboa@users.noreply.github.com>
2023-10-27 07:07:51 +0200

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -16,11 +16,12 @@
 #cd ..
 #rm -rf yay
  
-yay -S brave-bin bashmount create_ap dragon-drop etcher-bin flacon --noconfirm
-yay -S ios-mount-git ipscan jdownloader2 kde-servicemenus-flacconvert kde-servicemenus-imagetools kde-servicemenus-pdf kde-servicemenus-thunderbird-attachment --noconfirm
-yay -S kodi-addon-pvr-iptvsimple koreader-bin kwin-effects-burn-my-windows-git losslesscut-bin lsd luckybackup menulibre mocp-themes-git mp3check --noconfirm 
+yay -S brave-bin bashmount create_ap dragon-drop etcher-bin flacon ios-mount-git ipscan jdownloader2 --noconfirm
+#yay -S kde-servicemenus-flacconvert kde-servicemenus-imagetools kde-servicemenus-pdf kde-servicemenus-thunderbird-attachment kwin-effects-burn-my-windows-git --noconfirm
+yay -S kodi-addon-pvr-iptvsimple koreader-bin losslesscut-bin lsd luckybackup menulibre mocp-themes-git mp3check --noconfirm 
 yay -S mp3directcut mp3gain mugshot nerd-fonts-hack nomacs onlyoffice-bin peaclock puddletag rar rofi-greenclip rpi-imager stacer-bin subliminal tenacity --noconfirm
-yay -S thunar-custom-actions topgrade-bin usbtop ventoy-bin vlc-wayland-git zram-swap-git  --noconfirm
+yay -S topgrade-bin usbtop ventoy-bin vlc-wayland-git zram-swap-git  --noconfirm
+#yay -S thunar-custom-actions --noconfirm
 #sudo systemctl enable autofs
 
   

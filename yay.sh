ad7c3c72826c6049d5329550259b5599acbdac3b
holzboa <59257180+holzboa@users.noreply.github.com>
2023-10-27 07:51:47 +0200

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -16,12 +16,12 @@
 #cd ..
 #rm -rf yay
  
-yay -S brave-bin bashmount create_ap dragon-drop etcher-bin flacon ios-mount-git ipscan jdownloader2 --noconfirm
-#yay -S kde-servicemenus-flacconvert kde-servicemenus-imagetools kde-servicemenus-pdf kde-servicemenus-thunderbird-attachment kwin-effects-burn-my-windows-git --noconfirm
-yay -S kodi-addon-pvr-iptvsimple koreader-bin losslesscut-bin lsd luckybackup menulibre mocp-themes-git mp3check --noconfirm 
-yay -S mp3directcut mp3gain mugshot nerd-fonts-hack nomacs onlyoffice-bin peaclock puddletag rofi-greenclip rpi-imager stacer-bin subliminal tenacity --noconfirm
-yay -S topgrade-bin usbtop ventoy-bin vlc-wayland-git zram-swap-git  --noconfirm
-#yay -S thunar-custom-actions --noconfirm
+yay -S --needed brave-bin bashmount create_ap dragon-drop etcher-bin flacon ios-mount-git ipscan jdownloader2 --noconfirm
+#yay -S --needed kde-servicemenus-flacconvert kde-servicemenus-imagetools kde-servicemenus-pdf kde-servicemenus-thunderbird-attachment kwin-effects-burn-my-windows-git --noconfirm
+yay -S --needed kodi-addon-pvr-iptvsimple koreader-bin losslesscut-bin lsd luckybackup menulibre mocp-themes-git mp3check --noconfirm 
+yay -S --needed mp3directcut mp3gain mugshot nerd-fonts-hack nomacs onlyoffice-bin peaclock puddletag rofi-greenclip rpi-imager stacer-bin subliminal tenacity --noconfirm
+yay -S --needed topgrade-bin usbtop ventoy-bin vlc-wayland-git zram-swap-git  --noconfirm
+#yay -S --needed thunar-custom-actions --noconfirm
 #sudo systemctl enable autofs
 
   

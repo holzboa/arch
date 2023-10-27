96e9e0857a0bc7cefedb751c0bb255016a8ddc70
holzboa <59257180+holzboa@users.noreply.github.com>
2023-10-27 06:51:36 +0200

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+#set -e
+###############################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+###############################################################################
+#sudo pacman -Syu
 
-sudo pacman -Syu
-
-sudo pacman -S --needed git base-devel --noconfirm
+#sudo pacman -S --needed git base-devel --noconfirm
 
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd ..
-rm -rf yay
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si --noconfirm
+#cd ..
+#rm -rf yay
  
-yay -S autofs pamac-aur bashmount dragon-drag-and-drop --noconfirm
-yay -S gmusicbrowser-git ipscan jdownloader2 --noconfirm
-yay -S menulibre moc-unstable mocp-themes-git mp3check --noconfirm 
-yay -S mp3directcut mp3gain mugshot nerd-fonts-hack --noconfirm
-yay -S zramswap thunar-custom-actions lsd --noconfirm
-sudo systemctl enable autofs
+yay -S brave-bin bashmount create_ap dragon-drop etcher-bin flacon --noconfirm
+yay -S ios-mount-git ipscan jdownloader2 kde-servicemenus-flacconvert kde-servicemenus-imagetools kde-servicemenus-pdf kde-servicemenus-thunderbird-attachment --noconfirm
+yay -S kodi-addon-pvr-iptvsimple koreader-bin kwin-effects-burn-my-windows-git losslesscut-bin lsd luckybackup menulibre mocp-themes-git mp3check --noconfirm 
+yay -S mp3directcut mp3gain mugshot nerd-fonts-hack nomacs onlyoffice-bin peaclock puddletag rar rofi-greenclip rpi-imager stacer-bin subliminal tenacity --noconfirm
+yay -S thunar-custom-actions topgrade-bin usbtop ventoy-bin vlc-wayland-git zram-swap-git  --noconfirm
+#sudo systemctl enable autofs
 
-#yay -S flacon puddletag luckybackup usbtop
+  
